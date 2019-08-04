41612ce74d4f76d8e033d9614c54288bb5fde4a1
Emil Matyjaszewski <brc@brcdev.net>
2019-08-04 15:30:56 +0200

    Add project core

---
+++ src/main/java/net/brcdev/shopgui/ShopGuiPlugin.java
@@ -0,0 +1,10 @@
+package net.brcdev.shopgui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShopGuiPlugin extends JavaPlugin {
+
+  public static ShopGuiPlugin getInstance() {
+    return null;
+  }
+}
