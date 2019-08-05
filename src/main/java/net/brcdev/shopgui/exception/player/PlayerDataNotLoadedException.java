0aa00f2bd4e73b1379c1a61c0049aed89115a193
Emil Matyjaszewski <brc@brcdev.net>
2019-08-05 21:15:09 +0200

    Simplify PlayerDataNotLoadedException

--- src/main/java/net/brcdev/shopgui/exception/player/PlayerDataNotLoadedException.java
+++ src/main/java/net/brcdev/shopgui/exception/player/PlayerDataNotLoadedException.java
@@ -5,6 +5,5 @@ import org.bukkit.entity.Player;
 public class PlayerDataNotLoadedException extends Exception {
 
   public PlayerDataNotLoadedException(Player player) {
-    super(String.format("%s's player data wasn't loaded from the database yet", player.getName()));
   }
 }
