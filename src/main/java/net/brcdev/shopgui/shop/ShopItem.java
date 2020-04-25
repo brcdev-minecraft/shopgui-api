b5cc4cf05eed281033d18dab019f0159d4de1869
Emil Matyjaszewski <brc@brcdev.net>
2020-04-25 19:14:09 +0200

    Add more API methods to ShopItem
    See #1

--- src/main/java/net/brcdev/shopgui/shop/ShopItem.java
+++ src/main/java/net/brcdev/shopgui/shop/ShopItem.java
@@ -8,27 +8,74 @@ import static net.brcdev.shopgui.shop.ShopManager.ItemType;
 
 public class ShopItem {
 
+  public ShopItem(String id, ItemType type) {
+  }
+
+  public ShopItem(String id, ItemType type, ItemStack item, boolean unstack, int page, int slot, double buyPrice,
+                  double sellPrice, int originalStackSize) {
+  }
+
   public String getId() {
     return null;
+  }
+
+  public void setId(String id) {
   }
 
   public ItemStack getItem() {
     return null;
   }
 
+  public void setItem(ItemStack item) {
+  }
+
   public ItemStack getPlaceholder() {
     return null;
+  }
+
+  public void setPlaceholder(ItemStack placeholder) {
   }
 
   public ItemType getType() {
     return null;
   }
 
+  public void setType(ItemType type) {
+  }
+
   public int getPage() {
     return 0;
   }
 
+  public void setPage(int page) {
+  }
+
   public int getSlot() {
+    return 0;
+  }
+
+  public void setSlot(int slot) {
+  }
+
+  public double getBuyPrice() {
+    return 0;
+  }
+
+  public void setBuyPrice(double buyPrice) {
+  }
+
+  public double getBuyPrice(Shop shop, Player player, PlayerData playerData) {
+    return 0;
+  }
+
+  public double getSellPrice() {
+    return 0;
+  }
+
+  public void setSellPrice(double sellPrice) {
+  }
+
+  public double getSellPrice(Shop shop, Player player, PlayerData playerData) {
     return 0;
   }
 
